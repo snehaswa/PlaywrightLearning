1ca60211f75f11bd1adadf371184d2d562410d78
snehaswa <snehar@geekyants.com>
2026-01-19 12:19:44 +0530

    Locatorbyotherway

---
+++ PlaywrightLearning/tests/E-commPracticeOtherWay.spec copy.js
@@ -0,0 +1,89 @@
+const { test, expect } = require('@playwright/test');
+
+test('Open Rahul Shetty Academy login page', async ({ page }) => {
+
+    const products = page.locator('.card-body');
+    const productName = 'iphone 13 pro';
+    const email= 'Sneha@gmail.com';
+    console.log(`Number of products: ${await products.count()}`);
+    await page.goto('https://rahulshettyacademy.com/client/#/auth/login');
+    await expect(page).toHaveURL('https://rahulshettyacademy.com/client/#/auth/login');
+    //await page.locator("//input[@id='userEmail']").fill('Sneha@gmail.com');
+    await page.getByPlaceholder("email@example.com").fill('email');
+    //await page.locator("//input[@id='userPassword']").fill('Test@123');
+    await page.getByPlaceholder("enter your password").fill('Test@123');
+    //await page.locator("//*[@id='login']").click();
+    await page.getByRole('button',{name:"Login"}).click();
+    //await page.waitForLoadState('networkidle');
+    await page.locator(".card-body b").first().waitFor();
+    const titles = await page.locator('.card-body b').allTextContents();
+    console.log(titles);
+    await page.locator('.card-body').filter({ hasText: 'iphone 13 pro'}).getByRole('button', { name: 'Add To Cart' }).click();
+   // await expect(page.locator('.card-body b')).toHaveText
+   /* const count=await products.count();
+
+    for(let i=0;i<count;i++){
+        console.log( await products.nth(i).locator('b').textContent());
+
+        if(await products.nth(i).locator('b').textContent() === productName){
+            //add to cart
+            await products.nth(i).locator('text= Add To Cart').click();
+            break;
+           
+        }
+    }*/
+     
+    await page.locator("[routerlink*='cart']").click();
+    //sudo class locator by using text and tag to see the product is added or showing in the cart
+   const bool= await page.locator("h3:has-text('iphone 13 pro')").isVisible();
+   expect(bool).toBeTruthy();   
+    //const cartProducts= page.locator('.cartSection h3');
+    //const cartCount=await cartProducts.count();
+await page.locator("text=Checkout").click();
+await page.locator("input[value='4542 9931 9292 2293']").fill("1234 5678 9123 4567");
+//const Dropdown= page.locator('.input ddl');
+//await Dropdown.click();
+//const date= page.locator("input[placeholder='Select Expiry Date']");
+//await date.click();
+//await date.fill("12/25");
+/*const cvv= page.locator("input[type='text']");
+await cvv.click();
+await cvv.fill("123");
+const name= page.locator("input[placeholder='Name on Card']");
+await name.click();
+await name.fill("Sneha Swami");*/
+const country= page.locator("input[placeholder='Select Country']");
+await country.click();
+
+await page.locator("input[placeholder='Select Country']").type("ind",{delay:100});
+const dropdownOptions= page.locator(".ta-results");
+await dropdownOptions.waitFor();
+const optionsCount= await dropdownOptions.locator("button").count();
+for(let i=0;i<optionsCount;++i)
+    {
+
+    const text= await dropdownOptions.locator("button").nth(i).textContent();
+    if(text===" India"){
+        await dropdownOptions.locator("button").nth(i).click();
+        break;
+    }
+} 
+expect(page.locator(".user__name [type='text']").first()).toHaveText(email);
+   await page.locator(".action__submit").click();
+   await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
+   const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
+   console.log(orderId);
+ 
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = await page.locator("tbody tr");
+    for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
+});
--- PlaywrightLearning/tests/SpecialLocator.spec.js
+++ PlaywrightLearning/tests/SpecialLocator.spec.js
@@ -7,9 +7,13 @@ test('Special Locator Practice', async ({page})=>{
     await page.getByLabel("Check me out if you Love IceCreams!").check();
     await page.getByLabel("Employed").check();
     await page.getByLabel("Gender").selectOption("Female");
-    await page.getByPlaceholder("Email").fill("Sneha@gmail.com");
+    //await page.getByPlaceholder("Email").fill("Sneha@gmail.com"); 
+    // GetByLabel:It only works if the label tag is associated with input text box
     await page.getByPlaceholder("Password").fill("Test@123");
     await page.getByRole("button",{name:"Submit"}).click();
+    await page.getByText("Success! The Form has been submitted successfully!.").isVisible();
+    await page.getByRole("link",{name : "Shop"}).click();
+    await page.locator("app-card").filter({hasText: 'Nokia Edge'}).getByRole("button").click();
     
 
 });
